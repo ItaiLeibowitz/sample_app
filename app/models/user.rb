c4943015f38b95a599b1022d19da35a9e2bd1e76
Itai Leibowitz <Itai.Leibowitz@gmail.com>
2012-07-25 10:26:52 -0400

    finished chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-before_save { |user| user.email = email.downcase }
+ before_save { self.email.downcase! }
 
 
 
@@ -23,7 +23,7 @@ before_save { |user| user.email = email.downcase }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
 	  uniqueness: {case_sensitive: false}
 
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, length: {minimum: 6}
   validates :password_confirmation, presence: true
 
   
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Itai Leibowitz"
+    email    "Itai@stam.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+
